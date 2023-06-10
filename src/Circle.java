98b78d1169b3b5d9a24f41bdd7f1dee2f7eda939
vrena4567 <v.rena4567@gmail.com>
2023-06-10 11:19:25 +0200

    new constructor added

---
+++ src/Circle.java
@@ -0,0 +1,33 @@
+public class Circle {
+    // private instance variable, not accessible from outside this class
+    private double radius;
+    private String color;
+
+    // Constructors (overloaded)
+    /** Constructs a Circle instance with default value for radius and color */
+    public Circle() {  // 1st (default) constructor
+        radius = 1.0;
+        color = "red";
+    }
+
+    /** Constructs a Circle instance with the given radius and default color */
+    public Circle(double r) {  // 2nd constructor
+        radius = r;
+        color = "red";
+    }
+
+    public Circle(double radius, String color) {  // 3rd constructor
+        this.radius = radius;
+        this.color = color;
+    }
+
+    /** Returns the radius */
+    public double getRadius() {
+        return radius;
+    }
+
+    /** Returns the area of this Circle instance */
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
